336c65344baf1420602c2941487a724f12bf4bff
Karl James <dickensallinone@gmail.com>
2023-04-07 17:17:37 +0800

    update at 3 pm today

--- script/index.js
+++ script/index.js
@@ -3,7 +3,22 @@
 const toggleNavButton = document.querySelector('.menu-toggle');
 const menu = document.getElementById('menu');
 function toggleNav() {
-    menu.classList.toggle('menu-show')
+    if(menu.classList.contains('menu-visible')) {
+        menu.className = 'menu-show'
+    }
+    else if(menu.classList.contains('menu-show')) {
+        menu.className = 'menu-visible'
+    }
 }
 
 toggleNavButton.addEventListener('click',toggleNav);
+
+window.addEventListener('resize',function(e){
+    e.preventDefault();
+    const windowWidth = e.target.innerWidth;
+
+    if(windowWidth > 768 && menu.classList.contains('menu-show')) {
+        menu.className = 'menu-visible'
+    }else {return;}
+
+})
